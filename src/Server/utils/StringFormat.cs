6a93011a99ad7185733412e9bc87f15ed2ea75bc
Juan Carlos Espinoza <72110039+juancespinozazumbado@users.noreply.github.com>
2025-10-12 02:22:16 +0000

    add file config

--- src/Server/utils/StringFormat.cs
+++ src/Server/utils/StringFormat.cs
@@ -5,4 +5,11 @@ public static class StringFormat
 
     public static void FormatStringConfig(string config);
 
+    public static void AssignConfigFiles(object configOptions, File file = null){
+
+        if(file is null){
+            throw new Exception("File can't be null");
+        }
+    }
+
 }
