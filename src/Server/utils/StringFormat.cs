560ef805b3197eeeea5adfcb179b60cfcecfdf0b
Juan Carlos Espinoza <72110039+juancespinozazumbado@users.noreply.github.com>
2025-10-12 02:18:57 +0000

    retake formatter

--- src/Server/utils/StringFormat.cs
+++ src/Server/utils/StringFormat.cs
@@ -3,6 +3,12 @@ namespace Server.utils;
 public static class StringFormat 
 {
 
-    public static void FormatStringConfig(string config);
+    public static void FormatStringConfig(string config){
+        if(config is null or empty){
+            throw new Exception("string must not be an empty string");
+        }
+
+    }
+
 
 }
