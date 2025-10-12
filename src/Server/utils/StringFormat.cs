039cb6fa28843427369ed449c750dd197082f9ed
Juan Carlos Espinoza <72110039+juancespinozazumbado@users.noreply.github.com>
2025-10-12 02:28:22 +0000

    add private fields

--- src/Server/utils/StringFormat.cs
+++ src/Server/utils/StringFormat.cs
@@ -3,6 +3,9 @@ namespace Server.utils;
 public static class StringFormat 
 {
 
+    private static readonly ILoger<StringFormat> _logger;
+    private static final int LimitTraceHold = 500; 
+
     public static void FormatStringConfig(string config){
         if(config is null or empty){
             throw new Exception("string must not be an empty string");
