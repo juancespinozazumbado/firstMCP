c49941bd73fb6ca249754b5e1432d84c53d9b0b2
Juan Carlos Espinoza <72110039+juancespinozazumbado@users.noreply.github.com>
2025-10-12 02:30:30 +0000

    add GUID

--- src/Server/utils/StringFormat.cs
+++ src/Server/utils/StringFormat.cs
@@ -1,7 +1,13 @@
 namespace Server.utils;
 
+using system;
+
 public static class StringFormat 
 {
+
+   private static final String FormatterId = Guid.NewGuid();
+    
+    
 
     public static void FormatStringConfig(string config){
         if(config is null or empty){
