b8545da422e440ff8ff6c7f7e92134731996e520
Juan Carlos Espinoza <72110039+juancespinozazumbado@users.noreply.github.com>
2025-10-30 13:16:50 +0000

    add createBranche feature

--- src/Server/tools/IGithubService.cs
+++ src/Server/tools/IGithubService.cs
@@ -5,4 +5,10 @@ public interface IGithubService
     Task<int> GetTotalCommits(string owner, string repo);
     Task<int> GetTotalPullRequests(string owner, string repo);
     Task<int> GetTotalIssues(string owner, string repo);
+
+
+    //content to one feature
+    Task<bool> CreateNewBranche(string brancheName);
+
+
 }
